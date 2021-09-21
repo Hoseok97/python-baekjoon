ce0e05b403293c16c5f6e81077cae98ec95377bf
Hoseok97 <qusghtjr95@naver.com>
2021-09-21 18:39:16 +0900

    1974번

---
+++ 21_0921.py
@@ -0,0 +1,20 @@
+# baekjoon 알고리즘 단계별 문제
+# 기본 수학2
+# 소수 찾기
+
+N = int(input())
+numbers = map(int, input().split())
+sosu = 0
+for num in numbers:
+    error = 0
+    if num > 1:
+        for i in range(2,num):
+            if (num % i) == 0:
+                error += 1
+
+        if error == 0:
+            sosu += 1
+    
+
+print(sosu)
+            

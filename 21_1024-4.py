41e96d739e3799761a26be25a2ab2cb4d7a08e21
Hoseok97 <qusghtjr95@naver.com>
2021-10-24 21:22:43 +0900

    2579

---
+++ 21_1024-4.py
@@ -0,0 +1,23 @@
+# baekjoon 알고리즘 단계별 문제
+# 동적 계획법
+# 게단 오르기(2579번)
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+dp = [0 for _ in range(N+3)]
+arr = [0 for _ in range(N+3)]
+for k in range(1,N+1):
+    arr[k] = int(input())
+
+
+dp [1] = arr[1]
+dp [2] = arr[1] + arr[2]
+dp [3] = max(arr[1] + arr[3] ,arr[2] + arr[3])
+
+
+for i in range(4, N+1):
+    dp[i] = max( dp[i-3] + arr[i-1] + arr[i] ,  dp[i-2] + arr[i] ) 
+
+print(dp[N])

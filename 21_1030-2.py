dd66a5517050fc16bbf50de91d624c9fb2f2e0bf
Hoseok97 <qusghtjr95@naver.com>
2021-10-30 18:37:46 +0900

    2565

---
+++ 21_1030-2.py
@@ -0,0 +1,22 @@
+# baekjoon 알고리즘 단계별 문제
+# 동적 계획법
+# 전깃줄(2565번)
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+w = []
+w_b = []
+dp = [0 for i in range(n)]
+for i in range(n):
+    w.append(list(map(int, input().split())))
+w.sort(key = lambda x:x[0])
+for i in range(n):
+    w_b.append(w[i][1])
+for i in range(n):
+    for j in range(i):
+        if w_b[i] > w_b[j] and dp[i] < dp[j]:
+            dp[i] = dp[j]
+    dp[i] += 1
+print(n - max(dp))
